ff231b8db70fd2d4de9886a812a28eafaad9177a
jihannova <jihanazzahranova@gmail.com>
2022-10-10 16:18:28 +0700

    build done and successfully [skip ci]

--- repo.sh
+++ repo.sh
@@ -38,7 +38,7 @@ build () {
      #export ALLOW_MISSING_DEPENDENCIES=true
      export USE_GAPPS=true
      export USE_PIXEL_CHARGING=true
-     lunch nad_maple-user
+     lunch nad_maple_dsds-user
     #make bootimage -j8
     #make systemimage -j8
     #make vendorimage -j8
@@ -64,7 +64,7 @@ upload() {
 	if [ -f $(pwd)/out/target/product/map*/${ZIPNAME} ]; then
 		echo "Successfully Build"
         time rclone copy $(pwd)/out/target/product/${DEVICE}/${ZIPNAME} znxtproject:NusantaraProject/${DEVICE} -P
-		echo "Done."
+		echo "Build for maple now"
 		cd ~
 		rm ~/.git-credentials ~/.gitconfig
 		git config --global user.name "jihannova"
@@ -72,10 +72,10 @@ upload() {
 		echo "$TOKEN" > ~/.git-credentials
 		git config --global credential.helper store --file=~/.git-credentials
 		git clone ${TOKEN}/jihannova/Build-ROM -b 12.1 ${DEVICE}
-		time rclone copy znxtproject:NusantaraProject/ci/12.1/maple_dsds/repo.sh ${DEVICE} -P
-		time rclone copy znxtproject:NusantaraProject/ci/12.1/maple_dsds/.cirrus.yml ${DEVICE} -P
+		time rclone copy znxtproject:NusantaraProject/ci/12.1/maple/repo.sh ${DEVICE} -P
+		time rclone copy znxtproject:NusantaraProject/ci/12.1/maple/.cirrus.yml ${DEVICE} -P
 		cd ${DEVICE}
-        git add . && git commit -m "build done and successfully [skip ci]" && git push origin HEAD:12.1
+        git add . && git commit -m "build maple now" && git push origin HEAD:12.1
 	else
 		echo "Build failed"
 	fi
