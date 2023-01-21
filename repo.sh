315b81e94da033903d38c0c420fb72fd6bf6ae30
jihannova <jihanazzahranova@gmail.com>
2023-01-21 21:09:16 +0700

    get cacche lavender

---
+++ repo.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+    
+sync () {
+    cd ~/rom
+    time rclone copy znxtproject:ccache/$ROM_PROJECT/.repo.tar.zst ~/rom -P
+    time tar -xaf .repo.tar.zst
+    time rm -rf .repo.tar.zst
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+}
+
+com () {
+    #tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+    tar "-I zstd -1 -T2" -cf $1.tar.zst $1
+}
+
+get_repo () {
+  cd ~/rom
+  time com .repo 1
+  time rclone copy .repo.tar.* znxtproject:ccache/$ROM_PROJECT -P
+  time rm .repo.tar.*
+  ls -lh
+}
+
+build () {
+     cd ~/rom
+     . build/envsetup.sh
+     export CCACHE_DIR=~/ccache
+     export CCACHE_EXEC=$(which ccache)
+     export USE_CCACHE=1
+     ccache -M 50G
+     ccache -z
+     export BUILD_HOSTNAME=znxt
+     export BUILD_USERNAME=znxt
+     export TZ=Asia/Jakarta
+     lunch nad_lavender-user
+     #make bootimage vendorimage
+     make systemimage -j8
+     #mka bacon -j8
+}
+
+compile () {
+    sync
+    echo "done."
+    #get_repo
+    build
+}
+
+# Sorting final zip
+compiled_zip() {
+    DEVICE=$(ls $(pwd)/out/target/product)
+	ZIP=$(find $(pwd)/out/target/product/${DEVICE}/ -maxdepth 1 -name "*${DEVICE}*.zip" | perl -e 'print sort { length($b) <=> length($a) } <>' | head -n 1)
+	ZIPNAME=$(basename ${ZIP})
+}
+
+SF () {
+    cd ~/rom/out/target/product/${DEVICE}
+    project=xperia-xz-premium/NusantaraProject/tiramisu/${DEVICE}
+
+    # Upload
+    expect -c "
+    spawn sftp $SF_USERNAME@frs.sourceforge.net:/home/pfs/project/$project
+    expect \"yes/no\"
+    send \"yes\r\"
+    expect \"Password\"
+    send \"$SF_PASS\r\"
+    set timeout -1
+    expect \"sftp>\"
+    send \"put ${ZIPNAME}\r\"
+    expect \"Uploading\"
+    expect \"100%\"
+    expect \"sftp>\"
+    send \"bye\r\"
+    interact"
+}
+
+# Retry the ccache fill for 99-100% hit rate
+retry_cacche () {
+	export CCACHE_DIR=~/ccache
+	export CCACHE_EXEC=$(which ccache)
+	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
+	if [ $hit_rate -lt 99 ]; then
+	    git clone ${TOKEN}/jihannova/Build-ROM -b ccache ${DEVICE}
+		time rclone copy znxtproject:NusantaraProject/ci/cchace2/${DEVICE}/repo.sh ${DEVICE} -P && cd ${DEVICE}
+	    git add . && git commit -m "Retry Cache $(date -u +"%D %T%p %Z")"
+	    git push origin HEAD:ccache
+	else
+	    echo "Ccache is fully configured"
+	    git clone ${TOKEN}/jihannova/Build-ROM -b ccache ${DEVICE}
+		time rclone copy znxtproject:NusantaraProject/ci/${DEVICE}/repo.sh ${DEVICE} -P && cd ${DEVICE}
+	    git add . && git commit -m "Build $(date -u +"%D %T%p %Z")"
+	    git push origin HEAD:ccache
+	fi
+}
+
+upload() {
+	cd ~
+	rm ~/.git-credentials ~/.gitconfig
+	git config --global user.name "jihannova"
+	git config --global user.email "jihanazzahranova@gmail.com"
+	echo "$TOKEN" > ~/.git-credentials
+	git config --global credential.helper store --file=~/.git-credentials
+	if [ -f ~/rom/out/target/product/${DEVICE}/${ZIPNAME} ]; then
+		echo "Successfully Build"
+		time rclone copy ~/rom/out/target/product/${DEVICE}/${ZIPNAME} znxtproject:NusantaraProject/lavender -P
+	else
+		echo "Build failed"
+		retry_cacche
+	fi
+}
+
+cd ~/rom
+ls -lh
+compile &
+#sleep 55m
+sleep 60m
+kill %1
+compiled_zip
+upload
+
+# Lets see machine specifications and environments
+df -h
+free -h
+nproc
+cat /etc/os*
---
+++ retry.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Sorting final zip
+compiled_zip() {
+    DEVICE=$(ls $(pwd)/out/target/product)
+	ZIP=$(find $(pwd)/out/target/product/${DEVICE}/ -maxdepth 1 -name "*${DEVICE}*.zip" | perl -e 'print sort { length($b) <=> length($a) } <>' | head -n 1)
+	ZIPNAME=$(basename ${ZIP})
+}
+
+# Retry the ccache fill for 99-100% hit rate
+retry_ccache () {
+    export CCACHE_DIR=~/ccache
+    export CCACHE_EXEC=$(which ccache)
+	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
+	if [ $hit_rate -lt 99 ]; then
+		git clone ${TOKEN}/jihannova/Build-ROM -b nad-lavender clone && cd clone
+		git commit --allow-empty -m "Retry: Ccache loop $(date -u +"%D %T%p %Z")"
+		git push -q
+	else
+		echo "Ccache is fully configured"
+		git clone ${TOKEN}/jihannova/Build-ROM -b nad-lavender clone && cd clone
+		git commit --allow-empty -m "Retry Build $(date -u +"%D %T%p %Z")"
+		git push -q
+	fi
+}
+
+# Trigger retry only if compilation is not finished
+retry_event() {
+	if [ -f $(pwd)/out/target/product/${DEVICE}/${ZIPNAME} ]; then
+		echo "Successful Build"
+	else
+		retry_ccache
+	fi
+}
+
+cd ~/rom
+#sleep 59m
+sleep 117m
+rm ~/.git-credentials ~/.gitconfig
+git config --global user.name "jihannova"
+git config --global user.email "jihanazzahranova@gmail.com"
+echo "$TOKEN" > ~/.git-credentials
+git config --global credential.helper store --file=~/.git-credentials
+#sleep 7191
+compiled_zip
+retry_event
