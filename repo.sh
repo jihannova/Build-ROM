b5075c4eae6a7591acf610d4d1a12118abec7aed
jihannova <jihanazzahranova@gmail.com>
2023-01-31 15:57:55 +0700

    update CherishOS

--- repo.sh
+++ repo.sh
@@ -54,22 +54,15 @@ build_maple () {
 
 # Retry the ccache fill for 99-100% hit rate
 retry_cacche () {
-    cd ~/rom
+    cd ~ && ls
 	export CCACHE_DIR=~/ccache
 	export CCACHE_EXEC=$(which ccache)
+	ccache -s
 	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
-	if [ $hit_rate -lt 99 ]; then
-	    git clone ${TOKEN}/jihannova/Build-ROM -b ccache-cherish ${DEVICE}
-		time rclone copy znxtproject:CherishOS/ci/cchace2/repo.sh ${DEVICE} -P && cd ${DEVICE}
-		git add . && git commit -m "Retry Cacche $(date -u +"%D %T%p %Z")"
-	    git push origin HEAD:ccache-cherish
-	else
-	    echo "Ccache is fully configured"
-	    git clone ${TOKEN}/jihannova/Build-ROM -b ccache-cherish ${DEVICE}
-		time rclone copy znxtproject:CherishOS/ci/cchace1/repo.sh ${DEVICE} -P && cd ${DEVICE}
-	    git add . && git commit -m "get ccache Done at $(date -u +"%D %T%p %Z") [skip ci]"
-	    git push origin HEAD:ccache-cherish
-	fi
+	git clone ${TOKEN}/jihannova/Build-ROM -b ccache-cherish ${DEVICE}
+	time rclone copy znxtproject:CherishOS/ci/cchace2/repo.sh ${DEVICE} -P && cd ${DEVICE}
+	git add . && git commit -m "Retry Cacche $(date -u +"%D %T%p %Z")"
+	git push origin HEAD:ccache-cherish
 }
 
 upload() {
